b5d8a3ba9040ddf4caf2d8e426e8d310b7f09ac7
Esben Christensen <80604101+esbenchristensen@users.noreply.github.com>
2024-08-23 10:46:22 +0200

    Deleted unnecessary files and assets

--- FIskeProjekt _Gruppe2/fiskeprojekt-gruppe2-main-1-new-kopi/drizzle/0001_tired_the_initiative.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "id" TO "userId";--> statement-breakpoint
-ALTER TABLE "challenges" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "challenges" ADD COLUMN "user_id" serial NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "challenges" ADD CONSTRAINT "challenges_user_id_users_userId_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("userId") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
